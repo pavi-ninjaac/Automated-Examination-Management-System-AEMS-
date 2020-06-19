a1551ff99d3cc1a83c8a67ffbb1c3c1f234a772e
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-06-19 15:37:08 +0530

    added connectivity

--- app.ts
+++ app.ts
@@ -1,7 +1,27 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = Number(process.env.PORT) || 3000;
+
+mongoose.connect((process.env.DB_URL as string), {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.info('Connection established to mongo db.')
+});
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Server started at port ${port}`));
+// app.use('api/routes/auth');
+
+app.listen(port, () => console.log(`Server started at port ${port}.`));
