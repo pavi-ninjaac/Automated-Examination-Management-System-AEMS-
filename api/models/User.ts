1630a7a9d848e27e31f84259b6de4eaeb8db8a95
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-06-21 18:24:47 +0530

    add JWT

--- api/models/User.ts
+++ api/models/User.ts
@@ -8,10 +8,13 @@ export interface UserInterface extends Document {
 }
 
 const UserSchema = new Schema({
-  name: { type: String, trim: true, minlength: 3, required: true },
-  email: { type: String, trim: true, minlength: 10, unique: true, required: true },
+  name: { type: String, trim: true, minlength: 3, maxlength: 60, required: true },
+  email: { type: String, trim: true, minlength: 10, maxlength: 255, unique: true, required: true },
   phone: { type: Number, trim: true, minlength: 10, maxlength: 10, min: 5555555555, max: 9999999999 },
-  password: { type: String, required: true }
+  password: { type: String, minlength: 6, maxlength: 1024, required: true },
+  isVerified: { type: Boolean, default: false, required: true }
+}, {
+  timestamps: true
 });
 
 const User = mongoose.model<UserInterface>('User', UserSchema);
--- api/routes/auth.ts
+++ api/routes/auth.ts
@@ -10,20 +10,37 @@ const router = express.Router();
 const JSONParser = bodyParser.json();
 const saltRounds = 5;
 
-/* Just detect stupid hackers */
-router.get('/', (req, res) => {
-  res.sendStatus(400);
-  Logger.warn('AppInvalid route accessing detected:');
-  Logger.info(req);
-});
+/* Token validations - middleware */
+
+const generateAccessToken = ({ _id, email }: UserInterface): string => jwt.sign({
+  id: _id,
+  email: email
+}, (process.env.ACCESS_TOKEN_SECRET as string), { expiresIn: '30m' });
+
+const generateRefreshToken = ({ _id, email }: UserInterface): string => jwt.sign({
+  id: _id,
+  email: email
+}, (process.env.REFRESH_TOKEN_SECRET as string), { expiresIn: '1d' });
+
+const validateToken = (req: express.Request | any, res: express.Response, next: express.NextFunction) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) { return res.status(401).json({ message: 'access token is not found' }); }
+
+  jwt.verify(token, (process.env.ACCESS_TOKEN_SECRET as string), (err: any, deserializedInfo: any) => {
+    if (err) { return res.status(403).json({ message: 'invalid or expired access token' }); }
+    req.user = deserializedInfo;
+    next();
+  });
+}
 
 /* NEW USER */
-router.post('/create', JSONParser, async (req, res) => {
+router.post('/create', JSONParser, async (req: express.Request, res: express.Response) => {
   try {
     const existingUser = await User.find({ email: req.body.email }, { password: 0 });
     if (existingUser.length !== 0) {
       if (existingUser.length > 1) { Logger.error('Unique email violation detected!'); }
-      res.status(409).send({ message: 'email is already used' }); return;
+      return res.status(409).json({ message: 'email is already used' });
     }
     const hashedPassword: string = await bcrypt.hash(req.body.password, saltRounds);
     const newUser = new User({
@@ -33,7 +50,7 @@ router.post('/create', JSONParser, async (req, res) => {
       password: hashedPassword
     });
     await newUser.save();
-    res.status(201).send({ message: 'account created successfully' });
+    res.status(201).json({ message: 'account created successfully' });
   } catch (err) {
     res.sendStatus(500);
     Logger.error(err);
@@ -41,23 +58,23 @@ router.post('/create', JSONParser, async (req, res) => {
 });
 
 /* Login: Create and send a JWT */
-router.get('/start', JSONParser, (req, res) => {
+router.get('/start', JSONParser, (req: express.Request, res: express.Response) => {
   User.findOne({ email: req.body.email }, async (err, user: UserInterface) => {
-    if (err) { res.sendStatus(500); Logger.error('Error finding accounts'); return; }
-    if (!user) { res.status(401).send({ message: 'user not found' }); return; }
+    if (err) { Logger.error('Error finding accounts'); return res.sendStatus(500); }
+    if (!user) { return res.status(401).json({ message: 'user not found' }); }
     try {
       const isAuthentic = await bcrypt.compare(req.body.password, user.password);
-      if (!isAuthentic) { res.status(401).send({ message: 'wrong password' }); return; }
-      const token = jwt.sign({
-        id: user._id,
-        email: user.email
-      }, (process.env.SECRET as string), { expiresIn: '1h' });
-      res.status(200).send({ authenticated: true, accessToken: token, id: user._id, name: user.name });
+      if (!isAuthentic) { return res.status(401).json({ message: 'wrong password' }); }
+      const accessToken = generateAccessToken(user);
+      const refreshToken = generateRefreshToken(user);
+      res.status(200).json({ authenticated: true, accessToken: accessToken, refreshToken: refreshToken, name: user.name });
     } catch (err) {
       res.sendStatus(500);
       Logger.error(err);
     }
   });
-})
+});
+
+router.get('/userDetails', validateToken, (req: express.Request | any, res: express.Response) => { return res.send(req.user); });
 
 export default router;
