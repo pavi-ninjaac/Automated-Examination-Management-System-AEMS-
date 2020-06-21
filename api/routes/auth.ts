6a027fd80cd417e0af5dbbb5992af7dcb2d8844b
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-06-21 16:26:25 +0530

    get rid of callback hells & promises

--- api/routes/auth.ts
+++ api/routes/auth.ts
@@ -18,46 +18,45 @@ router.get('/', (req, res) => {
 });
 
 /* NEW USER */
-router.post('/create', JSONParser, (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds)
-    .then(function (hashedPassword: string) {
-      const newTest = new User({
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone,
-        password: hashedPassword
-      });
-      newTest.save()
-        .then(() => {
-          res.sendStatus(201);
-        })
-        .catch((error) => { Logger.error(error); });
-    })
-    .catch(err => {
-      Logger.error(`Unable to hash! ${err}`);
-      res.sendStatus(500);
+router.post('/create', JSONParser, async (req, res) => {
+  try {
+    const existingUser = await User.find({ email: req.body.email }, { password: 0 });
+    if (existingUser.length !== 0) {
+      if (existingUser.length > 1) { Logger.error('Unique email violation detected!'); }
+      res.status(409).send({ message: 'email is already used' }); return;
+    }
+    const hashedPassword: string = await bcrypt.hash(req.body.password, saltRounds);
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: hashedPassword
     });
+    await newUser.save();
+    res.status(201).send({ message: 'account created successfully' });
+  } catch (err) {
+    res.sendStatus(500);
+    Logger.error(err);
+  }
 });
 
-/* LOGIN attempt */
-router.get('/verify', JSONParser, (req, res) => {
-  User.findOne({ email: req.body.email }, (err, user: UserInterface) => {
+/* Login: Create and send a JWT */
+router.get('/start', JSONParser, (req, res) => {
+  User.findOne({ email: req.body.email }, async (err, user: UserInterface) => {
     if (err) { res.sendStatus(500); Logger.error('Error finding accounts'); return; }
-    if (!user) { res.send({ message: 'user not found' }); return; }
-    bcrypt.compare(req.body.password, user.password)
-      .then(function (authenticated) {
-        if (authenticated) {
-          const token = jwt.sign({
-            data: user.email
-          }, (process.env.SECRET as string), { expiresIn: '1h' });
-          res.send({ authenticated: true, accessToken: token, id: user._id, name: user.name });
-        } else {
-          res.send({ message: 'wrong password' });
-        }
-      })
-      .catch(err => {
-        Logger.error(err);
-      });
+    if (!user) { res.status(401).send({ message: 'user not found' }); return; }
+    try {
+      const isAuthentic = await bcrypt.compare(req.body.password, user.password);
+      if (!isAuthentic) { res.status(401).send({ message: 'wrong password' }); return; }
+      const token = jwt.sign({
+        id: user._id,
+        email: user.email
+      }, (process.env.SECRET as string), { expiresIn: '1h' });
+      res.status(200).send({ authenticated: true, accessToken: token, id: user._id, name: user.name });
+    } catch (err) {
+      res.sendStatus(500);
+      Logger.error(err);
+    }
   });
 })
 
