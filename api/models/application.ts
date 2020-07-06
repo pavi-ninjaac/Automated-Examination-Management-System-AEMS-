9569a41132e07916f47fa528fe1e239bd8981ad6
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-07-06 12:16:16 +0530

    added application schema

--- api/models/application.ts
+++ api/models/application.ts
@@ -1,30 +1,81 @@
-import mongoose from 'mongoose';
-
-const Schema = mongoose.Schema;
+import mongoose, { Schema, Document } from 'mongoose';
 
 const applicationSchema = new Schema({
-  name: { type: String, required: true },
-  fatherName: { type: String, required: true },
-  motherName: { type: String },
-  spouseName: { type: String },
+  name: { type: String, trim: true, required: true },
+  fatherName: { type: String, trim: true, required: true },
+  motherName: { type: String, trim: true },
+  spouseName: { type: String, trim: true },
   maritalStatus: { type: Boolean },
-  sex: { type: String },
-  dateOfBirth: { type: Date },
+  sex: { type: String, required: true },
+  dateOfBirth: { type: Date, required: true },
   placeOfBirth: { type: String },
   native: { type: String },
   religion: { type: String },
   caste: { type: String },
-  nationality: { type: String },
+  nationality: { type: String, required: true },
 
-  mobileNumber: { type: Number },
-  email: { type: String },
+  mobileNumber: { type: Number, required: true },
+  email: { type: String, trim: true, required: true },
 
-  permanentAddress: { type: String },
+  permanentAddress: { type: String, required: true },
   presentAddress: { type: String },
 
-  aadhaarNo: { type: String },
-  voterId: { type: String },
+  aadhaarNo: { type: String, required: true },
+  voterId: { type: String, required: true },
 
-  motherTongue: { type: String },
+  motherTongue: { type: String, required: true },
   knownLanguages: { type: String },
+
+  isDisabled: { type: Boolean, default: false },
+  isCriminal: { type: Boolean, default: false },
+  isExServiceMan: { type: Boolean, default: false },
+
+  identificationMarks: { type: String, required: true },
+
+  maxQualification: { type: String, required: true },
+  SSLC: {
+    institute: { type: String, trim: true, required: true },
+    address: { type: String, trim: true },
+    percentage: { type: Number, min: 50, max: 100 },
+    dateOfPassing: { type: Date, required: true }
+  },
+
+  HSC: {
+    institute: { type: String, trim: true, required: true },
+    address: { type: String, trim: true },
+    percentage: { type: Number, min: 50, max: 100 },
+    dateOfPassing: { type: Date, required: true }
+  },
+
+  college: {
+    degree: { type: String, trim: true, required: true },
+    department: { type: String, trim: true, required: true },
+    institute: { type: String, trim: true, required: true },
+    university: { type: String, trim: true, required: true },
+    address: { type: String, trim: true },
+    percentage: { type: Number, min: 50, max: 100 },
+    dateOfPassing: { type: Date, required: true }
+  },
+
+  documents: {
+    aadhaar: { type: String, required: true },
+    voter: { type: String, required: true },
+    SSLC: { type: String, required: true },
+    HSC: { type: String, required: true },
+    deg: { type: String, required: true },
+    photo: { type: String, required: true },
+    signature: { type: String, required: true }
+  },
+
+  additionalDetails: {
+    isWorking: { type: Boolean, required: true, default: false },
+    readRR: { type: Boolean, required: true, default: true },
+    readPP: { type: Boolean, required: true, default: true },
+  }
+}, {
+  timestamps: true
 });
+
+const Application = mongoose.model('Application', applicationSchema);
+
+export default Application;
