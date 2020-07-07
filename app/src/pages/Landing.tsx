a501e835e865cc6e00f7974d6f2fdcebd0846c85
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-07-07 16:55:51 +0530

    fix a stupid error

--- app/src/pages/Landing.tsx
+++ app/src/pages/Landing.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Button, Container, Row, Col } from 'react-bootstrap'
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import CenterContainer from '../containers/CenterContainer';
 import ProfIllustration from '../assets/images/professor.svg';
+
 
 function Landing() {
   return (
     <Container fluid="md">
       <CenterContainer>
         <Row>
-          <Col sm={12}>
-            <img src={ProfIllustration} style={{ display: 'block', minWidth: '30rem', maxWidth: '60%' }} alt="illustration" />
+          <Col sm={12} md={6} style={{ overflow: 'hidden' }}>
+            <img src={ProfIllustration} style={{ width: '100%', objectFit: 'cover' }} alt="illustration" />
           </Col>
-          <Col sm={12}>
+          <Col sm={12} md={6}>
             <Button variant="outline-primary" size="lg">Register</Button>
           </Col>
         </Row>
@@ -22,3 +27,21 @@ function Landing() {
 }
 
 export default Landing;
+
+
+/*
+return (
+  <Container fluid="md">
+    <CenterContainer>
+      <Row>
+        <Col sm={12}>
+          <img src={ProfIllustration} style={{ display: 'block', minWidth: '30rem', maxWidth: '60%' }} alt="illustration" />
+        </Col>
+        <Col sm={12}>
+          <Button variant="outline-primary" size="lg">Register</Button>
+        </Col>
+      </Row>
+    </CenterContainer>
+  </Container>
+);
+*/
