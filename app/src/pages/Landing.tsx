091946f01f88f656936681b6030b6a2a68141cef
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-07-07 13:19:37 +0530

    added back bootstrap

--- app/src/pages/Landing.tsx
+++ app/src/pages/Landing.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container } from 'react-bootstrap'
+import { Button, Container, Row, Col } from 'react-bootstrap'
 
 import CenterContainer from '../containers/CenterContainer';
 import ProfIllustration from '../assets/images/professor.svg';
@@ -8,8 +8,14 @@ function Landing() {
   return (
     <Container fluid="md">
       <CenterContainer>
-        <img src={ProfIllustration} style={{ display: 'block', minWidth: '30rem', maxWidth: '60%' }} alt="illustration" />
-        <Button variant="outline-primary" size="lg">Register</Button>
+        <Row>
+          <Col sm={12}>
+            <img src={ProfIllustration} style={{ display: 'block', minWidth: '30rem', maxWidth: '60%' }} alt="illustration" />
+          </Col>
+          <Col sm={12}>
+            <Button variant="outline-primary" size="lg">Register</Button>
+          </Col>
+        </Row>
       </CenterContainer>
     </Container>
   );
