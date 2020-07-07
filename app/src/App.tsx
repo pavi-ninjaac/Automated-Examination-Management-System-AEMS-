7348c3c7a76630c4e49dee6e4a9d4366400f0524
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-07-07 20:15:47 +0530

    changed to material ui and added auth pages

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import Landing from './pages/Landing';
+import SignUp from './pages/SignUp';
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import pink from '@material-ui/core/colors/pink';
 
 import './assets/styles/main.css';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+    secondary: pink
+  }
+});
+
 function App() {
   return (
-    <Landing />
+    <ThemeProvider theme={theme}>
+      <SignUp />
+    </ThemeProvider>
   );
 }
 
