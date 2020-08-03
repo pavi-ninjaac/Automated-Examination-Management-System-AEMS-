ab0f6659e43c8a41ecfd80790dc698ac93dd214f
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-03 13:05:05 +0530

    added padding to footer

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Typography } from '@material-ui/core'
 
 export default function Footer() {
   return (
-    <div style={{ marginTop: '2rem', backgroundColor: '#601e9e' }}>
+    <div style={{ marginTop: '2rem', backgroundColor: '#601e9e', padding: '1rem' }}>
       <Typography variant='h5'>&copy; Copyrights, 2020 - Government of Sikkim</Typography>
     </div>
   )
