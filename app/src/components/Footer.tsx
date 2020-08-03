d31566f716856ddcebf9397f638f060dc5c506de
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-08-03 13:51:06 +0530

    Improved footer

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Typography } from '@material-ui/core'
 export default function Footer() {
   return (
     <div style={{ marginTop: '2rem', backgroundColor: '#601e9e', padding: '1rem' }}>
-      <Typography variant='h5'>&copy; Copyrights, 2020 - Government of Sikkim</Typography>
+      <Typography variant='body1' style={{ color: 'white', textAlign: 'center' }}>&copy; Copyrights, 2020 - Government of Sikkim</Typography>
     </div>
   )
 }
